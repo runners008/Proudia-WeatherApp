8200770dfbf1cbfadbe6ed5f2948a6b3c73452e9
runnsss <runnsiriphuwanich@gmail.com>
2023-07-11 13:00:05 +0700

    Initial Commit

---
+++ Proudia Weather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Proudia Weather
+//
+//  Created by Runn Siriphuwanich on 11/7/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Proudia Weather/Proudia_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proudia_WeatherApp.swift
+//  Proudia Weather
+//
+//  Created by Runn Siriphuwanich on 11/7/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Proudia_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
